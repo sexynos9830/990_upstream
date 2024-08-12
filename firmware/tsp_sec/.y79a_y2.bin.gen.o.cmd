c333d2ccec87ac984eed14775b8adb5f4d7dd926
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2024-08-12 11:04:18 +0300

    firmware: push

---
+++ firmware/tsp_sec/.y79a_y2.bin.gen.o.cmd
@@ -0,0 +1,13 @@
+cmd_firmware/tsp_sec/y79a_y2.bin.gen.o := clang -Wp,-MD,firmware/tsp_sec/.y79a_y2.bin.gen.o.d -nostdinc -isystem /home/extremext/Android/toolchains/clang-r416183b/lib64/clang/12.0.5/include -I./arch/arm64/include -I./arch/arm64/include/generated  -I./include -I./arch/arm64/include/uapi -I./arch/arm64/include/generated/uapi -I./include/uapi -I./include/generated/uapi -include ./include/linux/kconfig.h -D__KERNEL__ -mlittle-endian -Qunused-arguments -D__ASSEMBLY__ --target=aarch64-linux-gnu --prefix=/home/extremext/Android/toolchains/gcc_4.9/bin/ --gcc-toolchain=/home/extremext/Android/toolchains/gcc_4.9 -no-integrated-as -Werror=unknown-warning-option -fno-PIE -DCONFIG_AS_LSE=1 -Wa,-gdwarf-2   -c -o firmware/tsp_sec/y79a_y2.bin.gen.o firmware/tsp_sec/y79a_y2.bin.gen.S
+
+source_firmware/tsp_sec/y79a_y2.bin.gen.o := firmware/tsp_sec/y79a_y2.bin.gen.S
+
+deps_firmware/tsp_sec/y79a_y2.bin.gen.o := \
+  include/linux/kconfig.h \
+    $(wildcard include/config/cpu/big/endian.h) \
+    $(wildcard include/config/booger.h) \
+    $(wildcard include/config/foo.h) \
+
+firmware/tsp_sec/y79a_y2.bin.gen.o: $(deps_firmware/tsp_sec/y79a_y2.bin.gen.o)
+
+$(deps_firmware/tsp_sec/y79a_y2.bin.gen.o):
